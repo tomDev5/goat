f023b66c2ba8ef36b96a967ebaecf72d30d03066
omer54463 <omer@lubin.ws>
2022-11-12 22:08:49 +0200

    REFACTOR Project.build() to ProjectBuilder.build_target_file()

--- goat/project/project.py
+++ goat/project/project.py
@@ -30,63 +30,8 @@ class Project:
     def __init__(self, configuration: ProjectConfiguration) -> None:
         self.configuration = configuration
 
-    def get_source_object_mapping(self, test: bool = False) -> dict[Path, Path]:
-        binary_source_files = self.configuration.source_directory.glob("**/*.cc")
-        test_source_files = self.configuration.test_directory.glob("**/*.cc")
-        source_files = (
-            chain(binary_source_files, test_source_files)
-            if test
-            else binary_source_files
-        )
-
-        source_object_mapping: dict[Path, Path] = {}
-
-        for source_file in source_files:
-            relative_source_file = source_file.relative_to(self.configuration.root_path)
-
-            object_file = (
-                self.configuration.object_directory
-                / relative_source_file.parent
-                / f"{relative_source_file.name}.o"
-            )
-
-            source_object_mapping[source_file] = object_file
-
-        return source_object_mapping
-
     def build(self, test: bool = False) -> None:
-        self.configuration.build_directory.mkdir(parents=True, exist_ok=True)
-        self.configuration.object_directory.mkdir(parents=True, exist_ok=True)
-        self.configuration.binary_directory.mkdir(parents=True, exist_ok=True)
-
-        source_object_mapping = self.get_source_object_mapping(test)
-
-        for source_file, object_file in source_object_mapping.items():
-            print(f"Compiling {source_file}")
-            object_file.parent.mkdir(parents=True, exist_ok=True)
-            result = ProjectBuilder.compile_object_file(
-                self.configuration,
-                source_file,
-                object_file,
-                test,
-            )
-
-            if result.returncode != 0:
-                print(result.stderr)
-                return
-
-        print(f"Linking {self.configuration.target_file(test)}")
-
-        result = ProjectBuilder.link_binary_file(
-            self.configuration,
-            list(source_object_mapping.values()),
-            self.configuration.target_file(test),
-            test,
-        )
-
-        if result.returncode != 0:
-            print(result.stderr)
-            return
+        ProjectBuilder.build_target_file(self.configuration, test)
 
     def run(self, test: bool = False) -> None:
         result = run(self.configuration.target_file(test))
--- goat/project/project_builder.py
+++ goat/project/project_builder.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from pathlib import Path
 from subprocess import CompletedProcess, run
 from goat.project.project_configuration import ProjectConfiguration
@@ -40,11 +41,13 @@ class ProjectBuilder:
     def linker_flags(
         cls,
         configuration: ProjectConfiguration,
-        binary_file: Path,
         test: bool,
     ) -> list[str]:
         test_linker_flags = [f"-l{library}" for library in cls.TEST_LINK_LIBRARIES]
-        return (test_linker_flags if test else []) + ["-o", str(binary_file)]
+        return (test_linker_flags if test else []) + [
+            "-o",
+            str(configuration.target_file(test)),
+        ]
 
     @classmethod
     def compile_object_file(
@@ -67,14 +70,80 @@ class ProjectBuilder:
         )
 
     @classmethod
-    def link_binary_file(
+    def link_target_file(
         cls,
         configuration: ProjectConfiguration,
         object_files: list[Path],
-        binary_file: Path,
         test: bool,
     ) -> CompletedProcess:
-        linker_flags = cls.linker_flags(configuration, binary_file, test)
+        linker_flags = cls.linker_flags(configuration, test)
         object_files_str = list(map(str, object_files))
 
         return run([configuration.compiler, *object_files_str, *linker_flags])
+
+    @classmethod
+    def get_source_object_mapping(
+        cls,
+        configuration: ProjectConfiguration,
+        test: bool = False,
+    ) -> dict[Path, Path]:
+        binary_source_files = configuration.source_directory.glob("**/*.cc")
+        test_source_files = configuration.test_directory.glob("**/*.cc")
+        source_files = (
+            chain(binary_source_files, test_source_files)
+            if test
+            else binary_source_files
+        )
+
+        source_object_mapping: dict[Path, Path] = {}
+
+        for source_file in source_files:
+            relative_source_file = source_file.relative_to(configuration.root_path)
+
+            object_file = (
+                configuration.object_directory
+                / relative_source_file.parent
+                / f"{relative_source_file.name}.o"
+            )
+
+            source_object_mapping[source_file] = object_file
+
+        return source_object_mapping
+
+    @classmethod
+    def build_target_file(
+        cls,
+        configuration: ProjectConfiguration,
+        test: bool = False,
+    ) -> None:
+        configuration.build_directory.mkdir(parents=True, exist_ok=True)
+        configuration.object_directory.mkdir(parents=True, exist_ok=True)
+        configuration.binary_directory.mkdir(parents=True, exist_ok=True)
+
+        source_object_mapping = cls.get_source_object_mapping(configuration, test)
+
+        for source_file, object_file in source_object_mapping.items():
+            print(f"Compiling {source_file}")
+            object_file.parent.mkdir(parents=True, exist_ok=True)
+            result = cls.compile_object_file(
+                configuration,
+                source_file,
+                object_file,
+                test,
+            )
+
+            if result.returncode != 0:
+                print(result.stderr)
+                return
+
+        print(f"Linking {configuration.target_file(test)}")
+
+        result = cls.link_target_file(
+            configuration,
+            list(source_object_mapping.values()),
+            test,
+        )
+
+        if result.returncode != 0:
+            print(result.stderr)
+            return
